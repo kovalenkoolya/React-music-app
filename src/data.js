8853e6b93bb898930995fc334c31769e31a86718
Olga Kovalenko <okovalenko.dev@gmail.com>
2020-12-23 19:36:53 +0300

    Add react music player

---
+++ src/data.js
@@ -0,0 +1,105 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#fbab4f"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#fbab4f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Ocean View",
+      cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      artist: "G Mills, Kyle McEvoy, Luke Otwell",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10320",
+      color: ["#d8a6bb", "#6b5580"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Frozen Firs",
+      cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      artist: "goosetaf, xander., Anbuu",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10313",
+      color: ["#d8a6bb", "#6b5580"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Finally Home",
+      cover: "https://chillhop.com/wp-content/uploads/2020/08/63d2d2cdabbc5df023603b5f47b2fb97963cb537-1024x1024.jpg",
+      artist: "Sitting Duck, Hoffy Beats, Otaam",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9785",
+      color: ["#d08c7a", "#6fccbf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+     
+        name: "Cozy Fire",
+        cover: "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
+        artist: "xander.",
+        audio: "https://mp3.chillhop.com/serve.php/?mp3=7955",
+        color: ["#d84c57", "#5a4999"],
+        id: uuidv4(),
+        active: false,
+      }
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
